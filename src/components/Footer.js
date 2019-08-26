4df7c29a514c34cee7042eb0707c9e3becd4d70f
Admin <admin@Admins-MacBook-Pro.local>
2019-08-26 12:00:24 +0600

    Added code for footer in file of Footer.js and aslo write some css code in main.scss file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,29 @@ class Footer extends Component {
     render() {
         return (
             <footer id="footer">
-                <div className="col-md-6">
-                    <div className="footer-menu">
-                        <ul className="list-inline">
-                            <li><a href="/">About us</a></li>
-                            <li><a href="/">Privacy policy</a></li>
-                            <li><a href="/">Terms & Condition</a></li>
-                            <li><a href="/">Blog</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="social-icon">
-                        <ul className="list-inline">
-                            <li><a href="/">Follows Us: </a></li>
-                            <li><a href="/"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="/"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="/"><i class="fab fa-linkedin-in"></i></a></li>
-                            
-                        </ul>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="footer-menu">
+                                <ul className="nav list-inline list-unstyled">
+                                    <li><a href="/">About us</a></li>
+                                    <li><a href="/">Privacy policy</a></li>
+                                    <li><a href="/">Terms & Condition</a></li>
+                                    <li><a href="/">Blog</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="social-icon pull-right">
+                                <ul className="list-inline list-unstyled float-right">
+                                    <li><a className="follows" href="/">Follows Us: </a></li>
+                                    <li><a href="/"><i className="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="/"><i className="fab fa-twitter"></i></a></li>
+                                    <li><a href="/"><i className="fab fa-linkedin-in"></i></a></li>
+
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </footer>
