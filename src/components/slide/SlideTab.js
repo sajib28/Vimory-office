f9cdee2aa6ddaab12f844efa2084e860e02dc191
Admin <admin@Admins-MacBook-Pro.local>
2019-09-02 16:14:13 +0600

    Added some css for svg icon

--- src/components/slide/SlideTab.js
+++ src/components/slide/SlideTab.js
@@ -110,82 +110,66 @@ class SlideTab extends Component {
                             </div>
                         </TabPanel>
                         <TabList>
-                            <Tab><div className="img-hover"><svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 46.831 46.831">
-                                <g id="Group_2051" data-name="Group 2051" transform="translate(-6688.701 -1456.503)">
-                                    <g id="Group_1999" data-name="Group 1999" transform="translate(6688.701 1456.503)">
-                                        <g id="Group_1998" data-name="Group 1998" transform="translate(2.061 2.061)">
-                                            <path id="Path_1729" data-name="Path 1729" className="cls-2" d="M28.243,39.443H26V8.348H8.2V6.1H28.243Z" transform="translate(5.1 3.266)" />
-                                            <path id="Path_1730" data-name="Path 1730" className="cls-2" d="M26.143,34.443H6.1V1.1H8.348V32.2h17.8Z" transform="translate(3.266 -1.1)" />
-                                            <rect id="Rectangle_1256" data-name="Rectangle 1256" className="cls-2" width="7.68" height="2.248" transform="translate(35.029 31.096)" />
-                                            <rect id="Rectangle_1257" data-name="Rectangle 1257" className="cls-2" width="7.68" height="2.248" transform="translate(0 9.366)" />
-                                        </g>
+                            <Tab><div className="img-hover slide-crop"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 34.362 33.278">
+                                <g id="Group_2106" data-name="Group 2106" transform="translate(-2027.163 -7307.624)">
+                                    <g id="Group_2105" data-name="Group 2105" transform="translate(2028.163 7308.624)">
+                                        <rect id="Rectangle_1346" data-name="Rectangle 1346" className="cls-1" width="24.748" height="24.643" transform="translate(0 0)" />
+                                        <rect id="Rectangle_1347" data-name="Rectangle 1347" className="cls-2" width="24.748" height="24.643" transform="translate(7.615 6.635)" />
                                     </g>
                                 </g>
-                            </svg></div><span>Crop</span>
+                            </svg>
+                            </div>
+                                <span>Crop</span>
                             </Tab>
                             <Tab>
-                                <div className="img-hover">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 44.833 44.833">
-                                        <g id="Group_2052" data-name="Group 2052" transform="translate(-727.334 -5456.334)">
-                                            <g id="Group_2001" data-name="Group 2001" transform="translate(727.334 5456.334)">
-                                                <g id="Group_2000" data-name="Group 2000" transform="translate(2.69 2.152)">
-                                                    <path id="Path_1732" data-name="Path 1732" className="cls-2" d="M22.012,32.824a12.94,12.94,0,0,1-10.76-20.085l1.793,1.255a10.558,10.558,0,0,0-1.793,6.1,10.76,10.76,0,1,0,10.76-10.76V7a12.912,12.912,0,0,1,0,25.824Z" transform="translate(4.529 3.401)" />
-                                                    <path id="Path_1733" data-name="Path 1733" className="cls-2" d="M14.412,35.324a12.912,12.912,0,1,1,0-25.824,13.421,13.421,0,0,1,10.76,5.559l-1.793,1.255a10.964,10.964,0,0,0-8.967-4.663,10.76,10.76,0,0,0,0,21.52A10.96,10.96,0,0,0,23.2,28.689l1.793,1.255A13.168,13.168,0,0,1,14.412,35.324Z" transform="translate(-1.5 5.385)" />
-                                                    <path id="Path_1734" data-name="Path 1734" className="cls-2" d="M15.133,27.024V24.872a10.76,10.76,0,1,0-10.76-10.76,10.068,10.068,0,0,0,1.614,5.559L4.193,20.747A12.308,12.308,0,0,1,2.4,14.112a12.912,12.912,0,1,1,25.824,0A13.012,13.012,0,0,1,15.133,27.024Z" transform="translate(-0.786 -1.2)" />
-                                                    <path id="Path_1735" data-name="Path 1735" className="cls-2" d="M16.635,14.587A12.389,12.389,0,0,1,10,17.456v-.179A10.734,10.734,0,0,1,11.793,11,9.343,9.343,0,0,1,16.635,14.587Z" transform="translate(5.243 6.575)" />
-                                                </g>
+                                <div className="img-hover slide-heart">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 33.439 29.943">
+                                        <g id="Group_2107" data-name="Group 2107" transform="translate(-2027.646 -7309.998)">
+                                            <g id="Group_2101" data-name="Group 2101" transform="translate(2028.646 7311)">
+                                                <path id="Path_21981" data-name="Path 21981" className="cls-1" d="M957.534,2411.328a8.215,8.215,0,0,0-8.942-7.78c-4.113.278-6.792,4.474-6.792,4.474s-2.747-4.1-6.865-4.24a8.391,8.391,0,0,0-8.81,8.085c-.212,4.3,2.7,8.117,5.056,10.707a52.684,52.684,0,0,0,10.812,8.723,54.094,54.094,0,0,0,10.666-9.092C954.965,2419.536,957.818,2415.615,957.534,2411.328Z" transform="translate(-926.114 -2403.527)" />
                                             </g>
                                         </g>
                                     </svg>
                                 </div>
-                                <span>Filter</span>
+                                <span>Heart</span>
                             </Tab>
-                            <Tab><div className="img-hover">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 71.739 71.74">
-                                    <g id="Group_2053" data-name="Group 2053" transform="translate(-6855.927 -1443.927)">
-                                        <g id="Group_2005" data-name="Group 2005" transform="translate(6855.927 1443.927)">
-                                            <g id="Group_2004" className="cls-2" data-name="Group 2004" transform="translate(16.63 21.268)">
-                                                <g id="Group_2002" data-name="Group 2002" transform="translate(0 18.553)">
-                                                    <path id="Path_1737" className="cls-2" data-name="Path 1737" d="M10.325,25.451a5.325,5.325,0,1,1,5.325-5.325A5.352,5.352,0,0,1,10.325,25.451Zm0-8.589a3.264,3.264,0,1,0,3.264,3.264A3.379,3.379,0,0,0,10.325,16.861Z" transform="translate(1.356 -14.8)" />
-                                                    <rect id="Rectangle_1258" data-name="Rectangle 1258" width="19.927" height="2.061" transform="translate(18.553 4.295)" />
-                                                    <rect id="Rectangle_1259" data-name="Rectangle 1259" width="4.81" height="2.061" transform="translate(0 4.295)" />
-                                                </g>
-                                                <g id="Group_2003" data-name="Group 2003">
-                                                    <rect id="Rectangle_1260" data-name="Rectangle 1260" width="4.81" height="2.061" transform="translate(33.67 4.295)" />
-                                                    <rect id="Rectangle_1261" data-name="Rectangle 1261" width="19.755" height="2.061" transform="translate(0 4.295)" />
-                                                    <path id="Path_1738" className="cls-2" data-name="Path 1738" d="M19.125,14.651a5.325,5.325,0,1,1,5.325-5.325A5.352,5.352,0,0,1,19.125,14.651Zm0-8.589a3.264,3.264,0,1,0,3.264,3.264A3.509,3.509,0,0,0,19.125,6.061Z" transform="translate(7.673 -4)" />
-                                                </g>
-                                            </g>
+                            <Tab><div className="img-hover slide-Slice">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 38.644 35.509">
+                                    <g id="Group_2108" data-name="Group 2108" transform="translate(-2024.672 -7307.772)">
+                                        <g id="Group_2102" data-name="Group 2102" transform="translate(2025.422 7308.522)">
+                                            <rect id="Rectangle_1336" data-name="Rectangle 1336" className="cls-1" width="7.357" height="21.256" transform="translate(7.357 12.754)" />
+                                            <rect id="Rectangle_1337" data-name="Rectangle 1337" className="cls-1" width="7.331" height="21.183" transform="translate(22.122 12.71)" />
+                                            <rect id="Rectangle_1338" data-name="Rectangle 1338" className="cls-2" width="7.357" height="21.256" transform="translate(0 0)" />
+                                            <rect id="Rectangle_1339" data-name="Rectangle 1339" className="cls-3" width="7.571" height="21.874" transform="translate(14.432 0)" />
+                                            <rect id="Rectangle_1340" data-name="Rectangle 1340" className="cls-2" width="7.571" height="21.874" transform="translate(29.573 0)" />
                                         </g>
                                     </g>
                                 </svg>
 
-
                             </div>
-                                <span>Adjustment</span></Tab>
+                                <span>Slice</span></Tab>
                             <Tab>
-                                <div className="img-hover">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 56.333 56.333">
-
-                                        <g id="Group_2054" data-name="Group 2054" transform="translate(-6955.667 -1453.667)">
-                                            <g id="Group_2007" data-name="Group 2007" transform="translate(6955.667 1453.667)">
-                                                <g id="Group_2006" data-name="Group 2006" transform="translate(8.788 8.788)">
-                                                    <path id="Path_1740" data-name="Path 1740" className="cls-2" d="M26.489,14.04h-2.7V6.6H16.8V3.9h9.689Z" transform="translate(12.268 -3.9)" />
-                                                    <path id="Path_1741" data-name="Path 1741" className="cls-2" d="M13.364,26.113H3.9V17.1H6.6v6.309h6.76Z" transform="translate(-3.9 12.644)" />
-                                                    <path id="Path_1742" data-name="Path 1742" className="cls-2" d="M6.6,14.04H3.9V3.9h9.464V6.6H6.6Z" transform="translate(-3.9 -3.9)" />
-                                                    <path id="Path_1743" data-name="Path 1743" className="cls-2" d="M26.489,26.113H16.8v-2.7h6.985V17.1h2.7Z" transform="translate(12.268 12.644)" />
-                                                    <path id="Path_1744" data-name="Path 1744" className="cls-2" d="M17.388,26.376A8.8,8.8,0,0,1,8.6,17.588,8.948,8.948,0,0,1,17.388,8.8a8.948,8.948,0,0,1,8.788,8.788A8.8,8.8,0,0,1,17.388,26.376Zm0-14.872a6.084,6.084,0,1,0,6.084,6.084A6.059,6.059,0,0,0,17.388,11.5Z" transform="translate(1.991 2.241)" />
-                                                </g>
+                                <div className="img-hover slide-flip">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 37.247 36.045">
+                                        <g id="Group_2109" data-name="Group 2109" transform="translate(-2026.583 -7306.263)">
+                                            <g id="Group_2100" data-name="Group 2100" transform="translate(2027.869 7309.088)">
+                                                <path id="Path_21979" data-name="Path 21979" className="cls-1" d="M1131.455,2403.37l-12.3,15.2,12.3,15.2Z" transform="translate(-1119.16 -2403.37)" />
+                                                <path id="Path_21980" data-name="Path 21980" className="cls-2" d="M1162.944,2403.37l12.3,15.2-12.3,15.2Z" transform="translate(-1140.565 -2403.37)" />
                                             </g>
                                         </g>
                                     </svg>
                                 </div>
-                                <span>Focus</span></Tab>
+                                <span>Flip</span></Tab>
                             <Tab>
-                                <div className="img-hover">
-                                    <i className="far fa-smile"></i>
+                                <div className="img-hover slide-kernel">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 40 40.276">
+                                        <g id="Group_2110" data-name="Group 2110" transform="translate(-2023.3 -7303.08)">
+                                            <path id="Path_21990" data-name="Path 21990" className="cls-1" d="M1717.091,2433.938l-18.591-18.719,18.591-18.719,18.591,18.719Z" transform="translate(326.21 4907.999)" />
+                                            <path id="Path_21991" data-name="Path 21991" className="cls-2" d="M1725.222,2432.507l-10.087-10.1,10.032-10.156,10.087,10.1Z" transform="translate(318.105 4900.326)" />
+                                        </g>
+                                    </svg>
                                 </div>
-                                <span>Emoji</span></Tab>
+                                <span>Kernel</span></Tab>
                         </TabList>
                     </div>
                 </Tabs>
