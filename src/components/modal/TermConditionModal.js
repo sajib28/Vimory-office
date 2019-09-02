e96f1f674db32cd8c1a2353212d10a3935c9df5b
Admin <admin@Admins-MacBook-Pro.local>
2019-09-02 13:48:17 +0600

    written some css code for about privacy term and comdition modal

--- src/components/modal/TermConditionModal.js
+++ src/components/modal/TermConditionModal.js
@@ -16,16 +16,15 @@ class TermConditionModal extends Component {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+        Terms and Conditions
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Terms and Conditions</h4>
+        
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ipsum, vitae adipisci cum molestiae harum, blanditiis eligendi maiores dolorum reiciendis odio. Magni, vero ipsa beatae quia illo quos consequatur pariatur corrupti repellendus molestias debitis perspiciatis similique ab magnam voluptates sequi saepe error cumque obcaecati totam iste! Autem ex similique minima?
         </p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam laudantium repudiandae facere odit dignissimos, sunt eos pariatur quisquam culpa aliquam consequatur natus. Nulla aut recusandae quasi quo natus voluptatem repellendus neque nam sequi deserunt alias eligendi nemo, sit ea illum quaerat minus totam placeat ducimus cum veritatis iste. Harum impedit laudantium quos omnis iste tempore itaque eveniet quo quibusdam unde! Necessitatibus architecto repellendus iusto impedit debitis consectetur doloremque corporis velit officiis ex obcaecati qui iure, ea nisi reprehenderit, corrupti possimus aperiam repellat. Nam perspiciatis similique distinctio nostrum eius, architecto, id ea autem dolorem corporis, voluptatibus laborum omnis ad earum tempora.</p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="danger" onClick={this.props.onHide}>Close</Button>
