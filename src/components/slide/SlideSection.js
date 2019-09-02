366e6c3736e4f4a0e2df3686dbf508e2c663c8d4
Admin <admin@Admins-MacBook-Pro.local>
2019-09-02 10:38:52 +0600

    Background with text component added in slide section

--- src/components/slide/SlideSection.js
+++ src/components/slide/SlideSection.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BackgroundWithText from '../CommonTools/BackgroundWithText';
 import mobileFrame from '../../assets/img/mobile-frame.png';
 import mobileFrameSlide from '../../assets/img/mobile-frame-slide.png';
+import BackgroundImage from '../../assets/img/template-bg.jpg';
 import SlideTab from './SlideTab';
 class Slide extends Component {
     render() {
         return (
             <section id="Slide">
-                <div id="SlideBg" className="photo-area">
-                    <div className="pic-overlay"></div>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="title">
-                                    <div className="title-inner">
-                                    <h2 className="wow fadeInUp">Slide</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               <BackgroundWithText id ="SlideBg" backgroundImage={BackgroundImage} title="Slide" shadowTitle="Slide"/>
                 <div className="content-area">
                     <div className="container">
                         <div className="row">
