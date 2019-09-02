30e277824801ba024744b0ae9dbd72834b4f7c57
Admin <admin@Admins-MacBook-Pro.local>
2019-09-02 18:05:37 +0600

    all modal text added into all modal

--- src/components/modal/AboutModal.js
+++ src/components/modal/AboutModal.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
-import { Modal,Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 class AboutModal extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
-    
-    render() {
+  // constructor(props){
+  //     super(props);
+  // }
 
-        return (
-            <Modal
-      {...this.props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-         About Us
+  render() {
+
+    return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            About Us
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ipsum, vitae adipisci cum molestiae harum, blanditiis eligendi maiores dolorum reiciendis odio. Magni, vero ipsa beatae quia illo quos consequatur pariatur corrupti repellendus molestias debitis perspiciatis similique ab magnam voluptates sequi saepe error cumque obcaecati totam iste! Autem ex similique minima?
-        </p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam laudantium repudiandae facere odit dignissimos, sunt eos pariatur quisquam culpa aliquam consequatur natus. Nulla aut recusandae quasi quo natus voluptatem repellendus neque nam sequi deserunt alias eligendi nemo, sit ea illum quaerat minus totam placeat ducimus cum veritatis iste. Harum impedit laudantium quos omnis iste tempore itaque eveniet quo quibusdam unde! Necessitatibus architecto repellendus iusto impedit debitis consectetur doloremque corporis velit officiis ex obcaecati qui iure, ea nisi reprehenderit, corrupti possimus aperiam repellat. Nam perspiciatis similique distinctio nostrum eius, architecto, id ea autem dolorem corporis, voluptatibus laborum omnis ad earum tempora.</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-        )
-    }
+        </Modal.Header>
+        <Modal.Body>
+          <p>APPILIAN, an app development company, located in Dhaka, Bangladesh, is committed to advancing the mobile revolution by improving the way people experience their Smartphone.</p>
+          <p>Our aim is to provide leading apps for iOS, Android and Web and let users personalize their contents in a better way. We have nurtured a team of best designers, artists, developers and technical experts, who are focused on delivering the most creative, innovative, profitable, and cutting edge applications and software.</p>
+          <p>With our extensive experience, we deliver top-notch software products to our clients around the world. We are always excited and thrilled to develop apps that change peoples’ lives.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 export default AboutModal;
--- src/components/modal/PrivacyPolicyModal.js
+++ src/components/modal/PrivacyPolicyModal.js
@@ -1,35 +1,74 @@
 import React, { Component } from 'react';
-import { Modal,Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 class PrivacyPolicyModal extends Component {
-    // constructor(props){
-    //     super(props);
-    // }
-    
-    render() {
+  // constructor(props){
+  //     super(props);
+  // }
 
-        return (
-            <Modal
-      {...this.props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Privacy Policy
+  render() {
+
+    return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Privacy Policy
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-      <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ipsum, vitae adipisci cum molestiae harum, blanditiis eligendi maiores dolorum reiciendis odio. Magni, vero ipsa beatae quia illo quos consequatur pariatur corrupti repellendus molestias debitis perspiciatis similique ab magnam voluptates sequi saepe error cumque obcaecati totam iste! Autem ex similique minima?
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            APPLIAN built the Vimory app as a Freemium app. This SERVICE is provided by APPLIAN at no cost and is intended for use as is.
         </p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam laudantium repudiandae facere odit dignissimos, sunt eos pariatur quisquam culpa aliquam consequatur natus. Nulla aut recusandae quasi quo natus voluptatem repellendus neque nam sequi deserunt alias eligendi nemo, sit ea illum quaerat minus totam placeat ducimus cum veritatis iste. Harum impedit laudantium quos omnis iste tempore itaque eveniet quo quibusdam unde! Necessitatibus architecto repellendus iusto impedit debitis consectetur doloremque corporis velit officiis ex obcaecati qui iure, ea nisi reprehenderit, corrupti possimus aperiam repellat. Nam perspiciatis similique distinctio nostrum eius, architecto, id ea autem dolorem corporis, voluptatibus laborum omnis ad earum tempora.</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-        )
-    }
+          <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
+          <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
+          <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Vimory unless otherwise defined in this Privacy Policy.</p>
+          <h5>Information Collection and Use</h5>
+          <p>For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request will be retained by us and used as described in this privacy policy.</p>
+          <p>The app does use third party services that may collect information used to identify you.</p>
+          <p>Link to privacy policy of third party service providers used by the app</p>
+          <ul>
+            <li><a href="">Google Play Services</a></li>
+            <li><a href="">App store</a></li>
+          </ul>
+          <h5>Log Data</h5>
+          <p>We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</p>
+          <h5>Cookies</h5>
+          <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.</p>
+          <p>This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+          <h5>Service Providers</h5>
+          <p>We may employ third-party companies and individuals due to the following reasons:</p>
+          <ul>
+            <li>To facilitate our Service</li>
+            <li>To provide the Service on our behalf</li>
+            <li>To perform Service-related services; or</li>
+            <li>To assist us in analyzing how our Service is used.</li>
+          </ul>
+          To facilitate our Service;
+    •	To provide the Service on our behalf;
+    •	To perform Service-related services; or
+    •	To assist us in analyzing how our Service is used.
+        <p>We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+          <h5>Security</h5>
+          <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the Internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+          <h5>Links to Other Sites</h5>
+
+          <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+          <h5>Children’s Privacy</h5>
+          <p>These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do the necessary actions.</p>
+          <h5>Changes to This Privacy Policy</h5>
+          <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+          <h5>Contact Us</h5>
+          <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at <a href="mailto:contact@appilian.com">contact@appilian.com</a></p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 export default PrivacyPolicyModal;
